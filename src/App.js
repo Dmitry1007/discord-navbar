46b48903ffd74a61ad321f75e26e4b1e934a5cf1
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-16 16:23:25 -0600

    Channel Bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import ChannelBar from "./ChannelBar";
 import SideBar from "./SideBar";
 
 function App() {
     return (
         <div className="flex">
             <SideBar />
+            <ChannelBar />
         </div>
     );
 }
--- src/SideBar.js
+++ src/SideBar.js
@@ -4,11 +4,14 @@ import { FaFire, FaPoo } from "react-icons/fa";
 const SideBar = () => {
     return (
         <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 shadow-lg">
-            <SideBarIcon icon={<FaFire size="28" />} />
-            <SideBarIcon icon={<BsPlus size="32" />} />
-            <SideBarIcon icon={<BsFillLightningFill size="20" />} />
-            <SideBarIcon icon={<FaPoo size="20" />} />
-            <SideBarIcon icon={<BsGearFill size="22" />} />
+            <SideBarIcon text="Fire" icon={<FaFire size="28" />} />
+            <SideBarIcon text="Plus" icon={<BsPlus size="32" />} />
+            <SideBarIcon
+                text="Thunder"
+                icon={<BsFillLightningFill size="20" />}
+            />
+            <SideBarIcon text="Poop" icon={<FaPoo size="20" />} />
+            <SideBarIcon text="Settings" icon={<BsGearFill size="22" />} />
         </div>
     );
 };
